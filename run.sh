89b3cddbd16788ce03496bbf8db32edfd8238ab6
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-01-24 19:21:35 +0100

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-DOCUMENT_TITLE="Rust Programming for Embedded Systems"
+DOCUMENT_TITLE="Rust programming for embedded systems"
 FILE_BASENAME="RUST-EMBEDDED"
 
 pandoc \
@@ -14,8 +14,11 @@ pandoc \
   --toc-depth=2 \
   --number-sections
 
-
-#pandoc -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" \
-#  --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 RUST-EMBEDDED.md \
-#  --metadata title="Rust Programming for Embedded Systems" --toc --toc-depth=2 --number-sections \
-#  --pdf-engine=lualatex -o output.pdf
+pandoc -N --variable "geometry=margin=1.2in"  "${FILE_BASENAME}.md" \
+  -o "${FILE_BASENAME}.pdf" \
+  --metadata title="${DOCUMENT_TITLE:-Untitled Document}" \
+  --toc \
+  --toc-depth=1 \
+  --number-sections \
+  --highlight-style tango \
+  --pdf-engine=lualatex
